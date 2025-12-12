63ea60cacb4e9571b0cac5bf71839a8fd610eee3
Artur <artur.czerwiak@gmail.com>
2025-12-12 08:22:13 +0100

    Refactor code

--- 02/components/HeaderCl.js
+++ 02/components/HeaderCl.js
@@ -1,7 +1,11 @@
 import React from 'react';
 export default class HeaderCl extends React.Component {
 	render() {
-		this.style = { fontWeight: 700, fontSize: '28px', color: 'rgb(20,130,50)' };
-		return <header style={this.style}>Mój komponent klasowy</header>;
+		const style = {
+			fontWeight: 700,
+			fontSize: '28px',
+			color: 'rgb(20,130,50)',
+		};
+		return <header style={style}>Mój komponent klasowy</header>;
 	}
 }
